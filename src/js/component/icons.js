ebb8aafd33263b4cf391ebf58ec628af70fec283
lilliana098 <lillianacc@uned.cr>
2021-03-13 03:52:31 +0000

    My first counter

---
+++ src/js/component/icons.js
@@ -0,0 +1,8 @@
+import fontawesome from '@fortawesome/fontawesome'
+import faClock from '@fortawesome/fortawesome-free-regular/faCloc'
+
+FontAwesome.config = {
+    autoReplaceSvg: 'nest'
+}
+
+fontawesome.library.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { Home } from "./component/home.js";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -8,8 +10,43 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="Clock">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="Four">{props.digitFour}</div>
+			<div className="Three">{props.digitThree}</div>
+			<div className="Two">{props.digitTwo}</div>
+			<div className="One">{props.digitOne}</div>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.PropTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+let counter = 0;
+setInterval(function() {
+	const Four = Math.floor(counter / 1000);
+	const Three = Math.floor(counter / 100);
+	const Two = Math.floor(counter / 10);
+	const One = Math.floor(counter / 1);
+	console.log(Four, Three, Two, One);
+	counter++;
+
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={One}
+			digitTwo={Two}
+			digitThree={Three}
+			digitFour={Four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
